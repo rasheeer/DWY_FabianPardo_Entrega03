d7c119f61da15f5d9eb991d46dfd26b288a1c466
Fabian Pardo Villarroel <pardo.fabian@hotmail.cl>
2019-11-28 20:46:45 -0300

    Se agrega serializers y se corrige editar

--- app/views.py
+++ app/views.py
@@ -2,22 +2,49 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-# -----------  para nuestos modelos --------------------
+# ============== para nuestos modelos ==============
 from .models import Cliente
-#from mantenedor.app.forms import CarreraForm
 from .forms import ClienteForm
 
+# EDITAR CLIENTE
+def editar_cliente(request, rut):
+    instancia= Cliente.objects.get(rut=rut)
+    form=  ClienteForm(instance=instancia)
+    if request.method=="POST":
+        form= ClienteForm(request.POST, instance=instancia)
+        if form.is_valid():
+            instancia= form.save(commit=False)
+            instancia.save()
+    return render(request, "app/editar.html",{'form':form})
 
+
+
+# ============== API REST ===============
+from .serializers import ClienteSerializer
+from rest_framework import generics
+
+class API_objets_details(generics.ListCreateAPIView):
+    queryset = Cliente.objects.all()
+    serializers_class = ClienteSerializer
+
+class API_objets_details(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cliente.objects.all()
+    serializers_class = ClienteSerializer
+# ========== TERMINO API REST ===========
+
+
+# ============== PAGINAS ===============
+# PAGINA DE ABOGADOS
 def abogados(request):
     return render(request, 'app/abogados.html', {})
 
+# PAGINA DE INICIO
 def index(request):
     return render(request, 'app/index.html', {})
+# ========== TERMINO PAGINAS ===========
 
 
-
-# ====== FUNCIONES ======
-
+# ============== FUNCIONES ===============
 # REGISTRO DE CLIENTE
 def registro_cliente(request):
     if request.method == "POST":
@@ -31,21 +58,11 @@ def registro_cliente(request):
         return render(request, 'app/registrocliente.html',
                       {'form': form})
 
+# LISTAR CLIENTES
 def listar_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, "app/listar.html", {'clientes': clientes})
         
-# EDITAR CLIENTE
-def editar_cliente(request, rut):
-    instancia= Cliente.objects.get(rut=rut)
-    form=  ClienteForm(instance=instancia)
-    if request.method=="POST":
-        form= ClienteForm(request.POST, instance=instancia)
-        if form.is_valid():
-            instancia= form.save(commit=False)
-            instancia.save()
-    return render(request, "app/editar.html",{'form':form})
-    
 
 # BORRAR CLIENTE
 def borrar_cliente(request, rut):
@@ -53,12 +70,11 @@ def borrar_cliente(request, rut):
     instancia.delete()
     return redirect('http://localhost:8000/listarfull')
 
-
-# CRUD COMPLETO 
+# LISTAR CLIENTES Y PERMITE EDITAR Y BORRAR
 def listar_cliente_full(request):
     clientes = Cliente.objects.all()
     return render(request, "app/listar_full.html", {'clientes': clientes})
-
+# ============== TERMINO DE FUNCIONES ===============
 
 
 
